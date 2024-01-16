25c43c3701a514e85df13c7b903e0d43ef851311
Nicholas Ragano <nragano@gmail.com>
2024-01-16 14:07:53 -0600

    updated launch settings for debug

--- src/JokeServer.java
+++ src/JokeServer.java
@@ -6,10 +6,11 @@
 //test
 class JokeClient {
 
-    private static int clientColorCount = 0;
-    //MODIFICATION: will track all colors sent and received by client
+
+    private static int clientJokeCount = 0;
+    //MODIFICATION: will track all Jokes sent and received by client
     //  uses an ArrayList to hold Arrays of strings, a new Array will be added to the ledger after each transaction
-    private static ArrayList<String[]> colorLedger = new ArrayList<String[]>();
+    private static ArrayList<String[]> JokeLedger = new ArrayList<String[]>();
     public static void main(String argv[]) {
         JokeClient cc = new JokeClient(argv);
         cc.run(argv);
@@ -38,72 +39,60 @@ class JokeClient {
         String userName = consoleIn.nextLine();
         System.out.println("Hi " + userName);
 
-        //get color loop, this takes new color requests until quit is typed
-        String colorFromClient = "";
+        //get Joke loop, this takes new Joke requests until quit is typed
+        String JokeFromClient = "";
         do{
-            System.out.println("Enter a color, or type quit to end: ");
-            colorFromClient = consoleIn.nextLine();
-            if(colorFromClient.indexOf("quit") < 0){
-                getColor(userName, colorFromClient, serverName);
+            System.out.println("Enter a Joke, or type quit to end: ");
+            JokeFromClient = consoleIn.nextLine();
+            if(JokeFromClient.indexOf("quit") < 0){
+                getJoke(userName, JokeFromClient, serverName);
             }
-        }while (colorFromClient.indexOf("quit") < 0);
+        }while (JokeFromClient.indexOf("quit") < 0);
         consoleIn.close(); //Added this as it was not closed in the original JokeClient code
         System.out.println("Cancelled by user request.");
-        System.out.println(userName + ", You completed " + clientColorCount + " color transactions");
+        System.out.println(userName + ", You completed " + clientJokeCount + " Joke transactions");
 
-        for(String[] s: colorLedger){
-            System.out.println("Transaction: " + (colorLedger.indexOf(s) + 1) + ", Color Sent: " + s[0] + ", Color Received: " + s[1]);
+        for(String[] s: JokeLedger){
+            System.out.println("Transaction: " + (JokeLedger.indexOf(s) + 1) + ", Joke Sent: " + s[0] + ", Joke Received: " + s[1]);
         }
     }
 
-    void getColor(String userName, String colorFromClient, String serverName){
+    void getJoke(String userName, String JokeFromClient, String serverName){
         try{
 
-            //create a ColorData object to pass the data to and from the server
-            ColorData colorObj = new ColorData();
-            colorObj.userName = userName;
-            colorObj.colorSentFromClient = colorFromClient;
-            colorObj.colorCount = clientColorCount;
+            //create a JokeData object to pass the data to and from the server
+            JokeData JokeObj = new JokeData();
+            JokeObj.userName = userName;
+            JokeObj.JokeSentFromClient = JokeFromClient;
+            JokeObj.JokeCount = clientJokeCount;
 
             //Establish a connection to the server
             Socket socket = new Socket(serverName, 45565);
-            System.out.println("\nWe have successfully connected to the ColorServer at port 45565");
+            System.out.println("\nWe have successfully connected to the JokeServer at port 45565");
 
             //establish an output stream using our socket
             OutputStream outStream = socket.getOutputStream();
             ObjectOutputStream objectOutStream = new ObjectOutputStream(outStream);
 
-            //serializes and sends the colordata object to the server
-            objectOutStream.writeObject(colorObj);
-            System.out.println("We have sent the serialized values to the ColorServer's server socket");
+            //serializes and sends the Jokedata object to the server
+            objectOutStream.writeObject(JokeObj);
+            System.out.println("We have sent the serialized values to the JokeServer's server socket");
 
             //Establish an input stream to listen for a response
             InputStream inStream = socket.getInputStream();
             ObjectInputStream objectInStream = new ObjectInputStream(inStream);
 
-            //Read the serialized ColorData response sent by the ColorServer
-            ColorData inObject = (ColorData) objectInStream.readObject();
+            //Read the serialized JokeData response sent by the JokeServer
+            JokeData inObject = (JokeData) objectInStream.readObject();
 
-            //record our colorcount in our client instance
-            clientColorCount = inObject.colorCount;
-            String[] colorRecordPair = new String[2];
-            colorRecordPair[0] = inObject.colorSentFromClient;
-            colorRecordPair[1] = inObject.colorSentFromServer;
+            //record our Jokecount in our client instance
+            clientJokeCount = inObject.JokeCount;
 
-            colorLedger.add(colorRecordPair);
-
-            System.out.println("\nFROM THE SERVER:");
-            System.out.println(inObject.messageToClient);
-            System.out.println("The color sent back is: " + inObject.colorSentFromServer);
-            System.out.println("The color count is: " + inObject.colorCount + "\n");
-
-            //Be sure to close our connection out when we are done
-            System.out.println("Closing the connection to the ColorServer.\n");
             socket.close();
 
         }catch(ConnectException CE){
             //will be thrown if server is not open, also thrown if request is refused (server backlog queue is exceeded)
-            System.out.println("\nOh no. The ColorServer refused our connection! Is it running?\n");
+            System.out.println("\nOh no. The JokeServer refused our connection! Is it running?\n");
             CE.printStackTrace();
 
         }catch(UnknownHostException UH){
@@ -112,7 +101,7 @@ class JokeClient {
             UH.printStackTrace();
 
         }catch(ClassNotFoundException CNF){
-            //This will be thrown if there is an issue with the Serialized ColorData class we are passing back and forth
+            //This will be thrown if there is an issue with the Serialized JokeData class we are passing back and forth
             CNF.printStackTrace();
 
         }catch(IOException IOE){
@@ -121,69 +110,36 @@ class JokeClient {
     }
 }
 
-class ColorData implements Serializable {
+class JokeData implements Serializable {
     //Allows this data to be serialized so that it can used by the object stream and sent over network
     String userName;
-    String colorSentFromClient;
-    String colorSentFromServer;
+    String JokeSentFromClient;
+    String JokeSentFromServer;
     String messageToClient;
-    int colorCount;
+    int JokeCount;
 }
 
-class ColorWorker extends Thread {
+class JokeWorker extends Thread {
     Socket sock;
 
     //Constructor
-    ColorWorker(Socket s){
-        //takes an arg of socket type and assigns it to local ColorWorker member, sock
+    JokeWorker(Socket s){
+        //takes an arg of socket type and assigns it to local JokeWorker member, sock
         sock = s;
     }
 
     public void run(){
         try{
-            //Creates an Object input stream allowing us to read in Serialized data
-            //  ObjectInputStream Deserializes this data sent to us
-            InputStream InStream = sock.getInputStream();
-            ObjectInputStream ObjectInStream = new ObjectInputStream(InStream);
 
-            //Responsible for reading the incoming serialized data and reconstructing the Colordata object
-            ColorData InObject = (ColorData) ObjectInStream.readObject();
 
-            //set up and prepare our output stream so we can send data back to the client
-            OutputStream outStream = sock.getOutputStream();
-            ObjectOutputStream objectOutStream = new ObjectOutputStream(outStream);
 
-            System.out.println("\nFROM THE CLIENT:\n");
-            System.out.println("Username: " + InObject.userName);
-            System.out.println("Color sent from the client: " + InObject.colorSentFromClient);
-            System.out.println("Connections count (State!): " + (InObject.colorCount + 1));
-
-            InObject.colorSentFromServer = getRandomColor();
-            InObject.colorCount++;
-            InObject.messageToClient = 
-                String.format("Thanks %s for sending the color %s", InObject.userName, InObject.colorSentFromClient);
-
-            //Serialize our ColorData Object with the updated data and send it back to the client
-            objectOutStream.writeObject(InObject);
-
-            System.out.println("Closing the client socket connection...");
+            System.out.println("Joke Here");
             sock.close(); //close the connection, we are done
 
-        } catch(ClassNotFoundException CNF){
-            CNF.printStackTrace();
-        } catch( IOException x){
+        }catch( IOException x){
             System.out.println("Server error.");
             x.printStackTrace();
         }
-    }
-
-    String getRandomColor(){
-        String[] colorArray = new String[]
-        {
-            "Red","Blue","Green","Yellow", "Magenta", "Silver", "Aqua", "Gray", "Peach", "Orange"
-        };
-        int randomArrayIndex = (int) (Math.random() * colorArray.length);
-        return (colorArray[randomArrayIndex]);
     }
 } 
 
@@ -192,10 +148,7 @@ public class JokeServer {
         int q_len = 6; /*Maximum number of requests to queue in the backlog, additional requests will be refused if full */
         int serverPort = 45565;
         Socket sock;
-
-        //split into two lines because I think it looks nicer
-        System.out.println("Nicholas Ragano's Color Server 1.0 starting, Listening on port " + serverPort + ".\n");
-
+        
         //Create our server socket using our port and allowed queue length
         ServerSocket serverSock = new ServerSocket(serverPort, q_len);
         System.out.println("Server open and awaiting connections...");
@@ -204,7 +157,7 @@ public class JokeServer {
             //Listen until a request comes in, accept it and spin up a worker thread to handle it
             sock = serverSock.accept(); //accept creates a new Socket and returns it
             System.out.println("Connection from: " + sock);
-            new ColorWorker(sock).start();
+            new JokeWorker(sock).start();
         }
     }
 }
